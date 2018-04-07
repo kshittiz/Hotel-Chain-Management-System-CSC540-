3e213274ef00840e5668aade4a283fcb7e29b213
Jubin <jajubina@ncsu.edu>
2018-04-07 04:11:33 -0400

    addPeople method added

--- src/dao/Manager.java
+++ src/dao/Manager.java
@@ -1,5 +1,28 @@
 package dao;
 
-public class Manager extends Staff {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+
+import org.json.JSONObject;
+
+public class Manager extends Staff {
+    public  int addPerson(JSONObject obj1) {
+        Connection c = Database.getConnection();
+        try {
+            
+            PreparedStatement exe = c.prepareStatement("insert into manager(pid, privilege) values(?,?)");
+            exe.setInt(1, obj1.getInt("pid"));
+            exe.setString(2, obj1.getString("privilege"));
+           exe.executeQuery();
+           
+           
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        
+        
+        return obj1.getInt("pid");
+    }
 }
