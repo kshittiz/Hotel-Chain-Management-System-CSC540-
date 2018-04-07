0ca2af4e97958d40c9c2c4fd8de2557dbd44d828
Jubin <jajubina@ncsu.edu>
2018-04-07 04:11:02 -0400

    addHotel method added

--- src/dao/Hotel.java
+++ src/dao/Hotel.java
@@ -1,5 +1,45 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+
+
+
+import java.sql.ResultSet;
+
+
 public class Hotel {
+    public  int addHotel(String name,String address) {
+        int hotelId=0; 
+                 Connection c = Database.getConnection();
+                 try {
+                     PreparedStatement exe = c.prepareStatement("insert into hotel(hotel_name,hotel_address) values(?, ?);", Statement.RETURN_GENERATED_KEYS);
+                     exe.setString(1, name);
+                     exe.setString(2, address);
+                     
+                     exe.executeQuery();
+                     ResultSet result = exe.getGeneratedKeys();
+                     if(result.next())
+                         hotelId=result.getInt(1);
+                     c.close();
+                 } catch (Exception e) {
+                     System.out.println(e);
+                 }
+                 return hotelId;
+             }
+    public static void main(String[] args) {
+
+        Hotel h = new Hotel();
+        
+
+      
+    
+      
+        int x= h.addHotel("Holiday Inn","Raleigh");
+        System.out.print(x);
+
+    }
 
 }
