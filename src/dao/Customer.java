b60afa20af9a73b112291bec05a6f39b9a186ce9
Jubin <jajubina@ncsu.edu>
2018-04-07 04:10:31 -0400

    addPeople method added for Customer

--- src/dao/Customer.java
+++ src/dao/Customer.java
@@ -1,5 +1,38 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.json.JSONObject;
+
 public class Customer extends People {
 
+    public  int addPerson(JSONObject obj1) {
+        
+        Connection c = Database.getConnection();
+        try {
+            ;
+            PreparedStatement exe = c.prepareStatement("insert into customer(pid,date_of_birth) values(?, ?)");
+            exe.setInt(1, obj1.getInt("pid"));
+            exe.setString(2, obj1.getString("date_of_birth"));
+           exe.executeQuery();
+           
+           
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return obj1.getInt("pid");
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
