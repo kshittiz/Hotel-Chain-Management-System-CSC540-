0e27dbc12c264123f642490c196265bac80ea585
Jubin <jajubina@ncsu.edu>
2018-04-07 04:09:51 -0400

    addPeople method added

--- src/dao/Cleaning.java
+++ src/dao/Cleaning.java
@@ -1,5 +1,45 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.json.JSONObject;
+
 public class Cleaning {
+    public  void addPerson(JSONObject obj1) {
+        
+        Connection c = Database.getConnection();
+        try {
+            PreparedStatement exe = c.prepareStatement("insert into cleaning_staff(pid, type) values(?, ?)");
+            exe.setInt(1, obj1.getInt("pid"));
+            exe.setString(2, obj1.getString("type"));
+           exe.executeQuery();
+           
+           
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        
+        
+        
+        
+    }
+ /*public static void main(String[] args) {
+
+Cleaning c = new Cleaning();
+JSONObject obj = new JSONObject();
+
+obj.put("pid", 15);
+obj.put("type", "Use cotton clothes");
+
+c.addPerson(obj);
+
+System.out.println(obj);
+
+
+
+}
+*/
 
 }
