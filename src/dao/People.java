8299ffbd838b127e7456a497d0b91b866f1c2670
Jubin <jajubina@ncsu.edu>
2018-04-06 21:03:29 -0400

    Id is generated

--- src/dao/People.java
+++ src/dao/People.java
@@ -3,22 +3,26 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Iterator;
 import org.json.JSONObject;
 
 public class People {
     public enum type{
-        customer,staff,chairman ;
+        customer,staff,chairman;
     }
     public  boolean addPerson(JSONObject obj1) {
         
 		        Connection c = Database.getConnection();
 		        try {
-		            PreparedStatement exe = c.prepareStatement("insert into people(name,ssn, type) values(?, ?, ?)");
+		            PreparedStatement exe = c.prepareStatement("insert into people(name,ssn, type) values(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 		            exe.setString(1, (String) (obj1.get("name")));
 		            exe.setInt(2, (int) (obj1.get("SSN")));
 		            exe.setString(3,(obj1.get("type")).toString());
 		            exe.executeQuery();
+		            ResultSet result = exe.getGeneratedKeys();
+		            if(result.next())
+		                System.out.println(result.getInt(1));
 		            c.close();
 		        } catch (Exception e) {
 		            System.out.println(e);
@@ -61,17 +65,17 @@ public class People {
         }
         return check;
     }
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
 
 		People p = new People();
 		JSONObject obj = new JSONObject();
 
 		obj.put("name", "Ojas");
-		obj.put("SSN", 997999889);
+		obj.put("SSN", 997019889);
 		obj.put("type", type.chairman);
 	
 		System.out.println(obj);
 		boolean x= p.addPerson(obj);
 
-	}*/
+	}
 }
