817c42f1b77594ee6a2eaebd3caef0cc8ac896e3
Jubin <jajubina@ncsu.edu>
2018-04-07 04:09:22 -0400

    addPeople method added

--- src/dao/FrontDesk.java
+++ src/dao/FrontDesk.java
@@ -1,5 +1,50 @@
 package dao;
 
-public class FrontDesk extends Staff {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+
+import org.json.JSONObject;
+
+public class FrontDesk extends Staff {
+    public  int addPerson(JSONObject obj1) {
+        
+                 Connection c = Database.getConnection();
+                 try {
+                     PreparedStatement exe = c.prepareStatement("insert into front_desk(pid, gender) values(?, ?)");
+                     exe.setInt(1, obj1.getInt("pid"));
+                     exe.setString(2, obj1.getString("gender"));
+                    exe.executeQuery();
+                    
+                    
+                 } catch (Exception e) {
+                     System.out.println(e);
+                 }
+        
+                 
+                 
+                 return obj1.getInt("pid");
+                 
+             }
+   /* public static void main(String[] args) {
+
+        FrontDesk f = new FrontDesk();
+        JSONObject obj = new JSONObject();
+
+        obj.put("pid", 15);
+        obj.put("gender", "male");
+       
+        f.addPerson(obj);
+    
+        System.out.println(obj);
+       
+  
+
+    }*/
+    
+    
+    
+    
+    
+    
 }
